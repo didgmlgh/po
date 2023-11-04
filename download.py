6a075e6ed54125c1abf1de8a16d5f1bf17e0b521
didgmlgh <149846669+didgmlgh@users.noreply.github.com>
2023-11-04 10:46:10 +0900

    Update download.py

--- download.py
+++ download.py
@@ -4,4 +4,4 @@ import numpy as np
 st.title('난 천재')
 dataframe = pd.DataFrame({'first column':[1,2,3,4],'second column':[10,20,30,40]})
 st.dataframe(dataframe,use_container_widthy=False)
-st.table(datasframe)
+st.table(datas\frame)
