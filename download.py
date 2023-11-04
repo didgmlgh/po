16791f580ef566a362904a15692ef748fe5cae35
didgmlgh <149846669+didgmlgh@users.noreply.github.com>
2023-11-04 10:48:25 +0900

    Update download.py

--- download.py
+++ download.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('난 천재')
-dataframe = pd.DataFrame({'first column':[1,2,3,4],'second column':[10,20,30,40]})
+dataframe = pd.DataFrame({'first column':[1,2,3,4],'second column':[10,20,30,40],})
 st.dataframe(dataframe,use_container_widthy=False)
 st.table(dataframe)
