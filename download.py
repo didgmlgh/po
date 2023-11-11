ad94a35b758ded7900281d893cf2a4797f220a83
didgmlgh <149846669+didgmlgh@users.noreply.github.com>
2023-11-11 10:11:40 +0900

    Update download.py

--- download.py
+++ download.py
@@ -14,7 +14,7 @@ col2.metric(label="유럽연합EUR",value="1335.82원",delta="11.44원")
 tab1,tab2=st.tabs(["Cat","Dog"])
 with tabl:
   st.header("A cat")
-  st.image(https://static.streamlit.io/examples/cat.jpg,width=200)
+  st.image("https://static.streamlit.io/examples/cat.jpg",width=200)
 with tab2:
   st.header("A dog")
-  st.image(https://static.streamlit.io/examples/dog.jpg,width=200)
+  st.image("https://static.streamlit.io/examples/dog.jpg",width=200)
